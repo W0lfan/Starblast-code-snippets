ef4f5b5f75a5180da7f7c10952471711677ac821
W0lfan <69418024+W0lfan@users.noreply.github.com>
2021-02-26 12:10:17 +0100

    Create stats_dueling_mod.js

---
+++ stats_dueling_mod.js
@@ -0,0 +1,460 @@
+var map = 
+"                                                                                                    \n"+
+"                                                                                                    \n"+
+"                                                                                                    \n"+
+"                                                                                                    \n"+
+"                                                                                                    \n"+
+"                                                                                                    \n"+
+"                                                                                                    \n"+
+"                                              99999999                                              \n"+
+"                                            999999999999                                            \n"+
+"                                         999999999999999999                                         \n"+
+"                                       9999999999999999999999                                       \n"+
+"                                      999999999999999999999999                                      \n"+
+"                                      999999999999999999999999                                      \n"+
+"                                     99999999999999999999999999                                     \n"+
+"                                   999999999999999999999999999999                                   \n"+
+"                                   999999999999999999999999999999                                   \n"+
+"                                   999999999999999999999999999999                                   \n"+
+"                                 9999999999999999999999999999999999                                 \n"+
+"                                 9999999999999999999999999999999999                                 \n"+
+"                              9999999999999999999999999999999999999999                              \n"+
+"                            99999999999999999999999999999999999999999999                            \n"+
+"                            99999999999999999999999999999999999999999999                            \n"+
+"                            99999999999999999999999999999999999999999999                            \n"+
+"                            99999999999999999999999999999999999999999999                            \n"+
+"                            99999999999999999999999999999999999999999999                            \n"+
+"                           9999999999999999999999999999999999999999999999                           \n"+
+"                        9999999999999999999999999999999999999999999999999999                        \n"+
+"                       999999999999999999999999999999999999999999999999999999                       \n"+
+"                      99999999999999999999999999999999999999999999999999999999                      \n"+
+"                      99999999999999999999999999999999999999999999999999999999                      \n"+
+"                      99999999999999999999999999999999999999999999999999999999                      \n"+
+"                     9999999999999999999999999999999999999999999999999999999999                     \n"+
+"                     9999999999999999999999999999999999999999999999999999999999                     \n"+
+"                    9999999999999999999999999999    9999999999999999999999999999                    \n"+
+"                   999999999999999999999999999        999999999999999999999999999                   \n"+
+"                   9999999999999999999999999            9999999999999999999999999                   \n"+
+"                  9999999999999999999999999              9999999999999999999999999                  \n"+
+"                 99999999999999999999999999              99999999999999999999999999                 \n"+
+"                9999999999999999999999999                  9999999999999999999999999                \n"+
+"               99999999999999999999999999                  99999999999999999999999999               \n"+
+"              99999999999999999999999999                    99999999999999999999999999              \n"+
+"             999999999999999999999999999                    999999999999999999999999999             \n"+
+"            9999999999999999999999999999                    9999999999999999999999999999            \n"+
+"           99999999999999999999999999999                    99999999999999999999999999999           \n"+
+"           9999999999999999999999999999                      9999999999999999999999999999           \n"+
+"           999999999999999999999999999                        999999999999999999999999999           \n"+
+"           999999999999999999999999999                        999999999999999999999999999           \n"+
+"           99999999999999999999999999                          99999999999999999999999999           \n"+
+"           99999999999999999999999999                          99999999999999999999999999           \n"+
+"           99999999999999999999999999                          99999999999999999999999999           \n"+
+"           99999999999999999999999999                          99999999999999999999999999           \n"+
+"           99999999999999999999999999                          99999999999999999999999999           \n"+
+"           99999999999999999999999999                          99999999999999999999999999           \n"+
+"           999999999999999999999999999                        999999999999999999999999999           \n"+
+"           999999999999999999999999999                        999999999999999999999999999           \n"+
+"           9999999999999999999999999999                      9999999999999999999999999999           \n"+
+"           99999999999999999999999999999                    99999999999999999999999999999           \n"+
+"            9999999999999999999999999999                    9999999999999999999999999999            \n"+
+"             999999999999999999999999999                    999999999999999999999999999             \n"+
+"              99999999999999999999999999                    99999999999999999999999999              \n"+
+"               99999999999999999999999999                  99999999999999999999999999               \n"+
+"                9999999999999999999999999                  9999999999999999999999999                \n"+
+"                 99999999999999999999999999              99999999999999999999999999                 \n"+
+"                  9999999999999999999999999              9999999999999999999999999                  \n"+
+"                   9999999999999999999999999            9999999999999999999999999                   \n"+
+"                   999999999999999999999999999        999999999999999999999999999                   \n"+
+"                    9999999999999999999999999999    9999999999999999999999999999                    \n"+
+"                     9999999999999999999999999999999999999999999999999999999999                     \n"+
+"                     9999999999999999999999999999999999999999999999999999999999                     \n"+
+"                      99999999999999999999999999999999999999999999999999999999                      \n"+
+"                      99999999999999999999999999999999999999999999999999999999                      \n"+
+"                      99999999999999999999999999999999999999999999999999999999                      \n"+
+"                       999999999999999999999999999999999999999999999999999999                       \n"+
+"                        9999999999999999999999999999999999999999999999999999                        \n"+
+"                           9999999999999999999999999999999999999999999999                           \n"+
+"                            99999999999999999999999999999999999999999999                            \n"+
+"                            99999999999999999999999999999999999999999999                            \n"+
+"                            99999999999999999999999999999999999999999999                            \n"+
+"                            99999999999999999999999999999999999999999999                            \n"+
+"                            99999999999999999999999999999999999999999999                            \n"+
+"                              9999999999999999999999999999999999999999                              \n"+
+"                                 9999999999999999999999999999999999                                 \n"+
+"                                 9999999999999999999999999999999999                                 \n"+
+"                                   999999999999999999999999999999                                   \n"+
+"                                   999999999999999999999999999999                                   \n"+
+"                                   999999999999999999999999999999                                   \n"+
+"                                     99999999999999999999999999                                     \n"+
+"                                      999999999999999999999999                                      \n"+
+"                                      999999999999999999999999                                      \n"+
+"                                       9999999999999999999999                                       \n"+
+"                                         999999999999999999                                         \n"+
+"                                            999999999999                                            \n"+
+"                                              99999999                                              \n"+
+"                                                                                                    \n"+
+"                                                                                                    \n"+
+"                                                                                                    \n"+
+"                                                                                                    \n"+
+"                                                                                                    \n"+
+"                                                                                                    \n"+
+"                                                                                                    ";
+
+
+
+this.options = {
+  // see documentation for options reference
+  root_mode: "",
+  map_size: 100,
+  custom_map: map,
+  weapons_store: false,
+  starting_ship: 800,
+  asteroids_strength: 1000000,
+  soundtrack: "red_mist.mp3",
+  map_name: "[ || ] team's dueling server",
+};
+
+var reset_button = {
+  id: "reset_button",
+  position: [5,30,9,14],
+  clickable: true,
+  shortcut: "R",
+  visible: true,
+  components: [
+    { type: "box",position:[0,0,100,40],stroke:"#5F5F5F", fill:"#CACACA",width:5},
+    { type: "text",position:[10,5,80,30],value:"RESET [R]",color:"#000000"},
+    ]
+};
+var ready = {
+  id: "ready",
+  position: [5,38,9,14],
+  clickable: true,
+  shortcut: "O",
+  visible: true,
+  components: [
+    { type: "box",position:[0,0,100,40],stroke:"#5F5F5F", fill:"",width:5},
+    { type: "text",position:[10,5,80,30],value:"",color:"#000000"},
+    ]
+};
+
+var infos = {
+  id: "infos",
+  position: [22,-5,55,75],
+  clickable: false,
+  visible: true,
+  components: [
+    { type: "text",position:[0,0,100,40],value:"",color:""},
+    { type: "text",position:[10,17,75,25],value:"Use stats to upgrade your ship",color:"#CDE"}
+    ]
+};
+
+var actualize_infos = function(ship,message, color) {
+  infos.components[0].value = message;
+  infos.components[0].color = color;
+  ship.setUIComponent(infos);
+};
+var spawn = function(ship) {
+  ship.set({
+    stats: 00000000,
+    x:500,
+    y:0
+  });
+};
+var invulnerable = function(ship) {
+  ship.set({
+    invulnerable: 100,
+    stats: 00000000,
+    crystals: (((Math.trunc(ship.type / 100) * 100) /2) / 10),
+    generator: 0
+  });
+  change_component(ship, "NOT READY [O]", "#A71111");
+};
+
+var respawn = function(ship) {
+  ship.set({
+    x:500,
+    y:0
+  });
+  ship.custom.invulnerable = true;
+  ship.custom.ready = false;
+};
+game.custom.fighters = 0;
+
+
+var reset = function(ship) {
+  game.custom.fighters = 0;
+  ship.set({x:500,y:0});
+  ship.custom.ready = false;
+  ship.custom.invulnerable = true;
+};
+this.tick = function(game) {
+  if (game.step === 0) {
+    game.custom.fighters = 0;
+  }
+  if (game.step % 60 === 0) {
+    for (let ship of game.ships) {
+      if (ship.custom.start_count === true) {
+        if (ship.custom.seconds > 0) {
+          ship.custom.seconds--;
+        }
+        else {
+          actualize_infos(ship,"", "#CDE" );
+          ship.custom.start_count = false;
+          ship.custom.seconds+=10;
+        }
+      }
+    }
+  }
+  if (game.step % 15 === 0) {
+    for (let ship of game.ships) {
+      ship.setUIComponent(infos);
+      if (ship.custom.start !== true) {
+        ship.custom.start = true;
+        ship.custom.ship_chosable = true;
+        ship.custom.invulnerable = true;
+        ship.custom.ready = false;
+        set_crystals(ship);
+        spawn(ship);
+        ship.setUIComponent(reset_button);
+        ship.setUIComponent(ready);
+        change_component(ship, "NOT READY [O]", "#A71111");
+        ship.custom.score = 0;
+        ship.custom.start_count = true;
+        ship.custom.seconds = 10;
+      }
+      check_stats(ship);
+      set_score(ship);
+      if (ship.custom.invulnerable === true) {
+        invulnerable(ship);
+      }
+      if (ship.alive !== true) {
+        if (ship.custom.ready === true) {
+          ship.custom.ready = false;
+          change_component(ship,"NOT READY [O]", "#A71111" );
+        }
+      }
+      if (game.custom.fighters >= 2) {
+        actualize_infos(ship,"A duel is running right now!" , "#DE0000" );
+      } else {
+        actualize_infos(ship,"There is no duel going on right now! " + game.custom.fighters + "/2", "#109B04" );
+      }
+    }
+  }
+};
+var set_stats = function(ship) {
+  ship.set({
+    stats: 11111111 * Math.trunc(ship.tier / 100)
+  });
+};
+var set_score = function(ship) {
+  ship.set({
+    score: ship.custom.score
+  });
+};
+var reset = function(ship) {
+  ship.set({
+    type: 101,
+    crystals: 5
+  });
+};
+
+
+var set_crystals = function(ship) {
+  ship.set({
+    crystals:
+     5
+  });
+};
+
+
+
+var check_stats = function(ship) {
+  if (ship.custom.ready !== true) {
+  if ( ship.stats === 10000000) {
+    ship.set({
+      type: 
+      Math.trunc(ship.type  / 100) * 100 + 101,
+      crystals: (((Math.trunc(ship.type / 100) * 100) /2) / 10) + 5
+    });
+    }
+  else if ( ship.stats === 1000000 ) {
+    ship.set({
+      type: Math.trunc(ship.type  / 100) * 100 + 102,
+      crystals: (((Math.trunc(ship.type / 100) * 100) /2) / 10) + 5
+    });
+  }
+  else if ( ship.stats === 100000 ) {
+    ship.set({
+      type: 
+      Math.trunc(ship.type  / 100) * 100 + 103,
+      crystals: (((Math.trunc(ship.type / 100) * 100) /2) / 10) + 5
+    });
+    }
+  else if ( ship.stats === 10000 ) {
+    ship.set({
+      type: Math.trunc(ship.type  / 100) * 100 + 104,
+      crystals: (((Math.trunc(ship.type / 100) * 100) /2) / 10) + 5
+    });
+    }
+  else if ( ship.stats === 1000 ) {
+    ship.set({
+      type: 
+      Math.trunc(ship.type  / 100) * 100 + 105,
+      crystals: (((Math.trunc(ship.type / 100) * 100) /2) / 10) + 5
+    });
+    }
+  else if ( ship.stats === 100 ) {
+    ship.set({
+      type: Math.trunc(ship.type  / 100) * 100 + 106,
+      crystals: (((Math.trunc(ship.type / 100) * 100) /2) / 10) + 5
+    });
+    }
+  else if ( ship.stats === 10 ) {
+    ship.set({
+      type: Math.trunc(ship.type  / 100) * 100 + 107,
+      crystals: (((Math.trunc(ship.type / 100) * 100) /2) / 10) + 5
+    });
+    }
+  else if ( ship.stats === 1 ) {
+    ship.set({
+      type: Math.trunc(ship.type  / 100) * 100 + 108,
+      crystals: (((Math.trunc(ship.type / 100) * 100) /2) / 10) + 5
+    });
+    }
+    if (ship.stats > 11000000 && ship.type > 100 && ship.type < 200) {
+      ship.set({
+        crystals: 0,
+        stats: 0
+      })
+    }
+  }
+  if (ship.type < 200 && ship.stats > 11000000) {
+    ship.set({
+      crystals: 5,
+      stats: 0
+    })
+  }
+  else if (ship.type < 300 && ship.type > 200 && ship.stats > 22220000) {
+    ship.set({
+      crystals: 10,
+      stats: 0
+    })
+  }
+  else if (ship.type < 400 && ship.type > 300 && ship.stats > 33333300) {
+    ship.set({
+      crystals: 15,
+      stats: 0
+    })
+  }
+  else if (ship.type < 500 && ship.type > 400 && ship.stats > 44444440) {
+    ship.set({
+      crystals: 10,
+      stats: 0
+    })
+  }
+  else if (ship.type < 600 && ship.type > 500 &&  ship.stats > 55555555) {
+    ship.set({
+      crystals: 10,
+      stats: 0
+    })
+  }
+  else if (ship.type < 700 && ship.type > 600 && ship.stats > 66660000) {
+    ship.set({
+      crystals: 10,
+      stats: 0
+    })
+  }
+};
+
+
+var ready_or_not = function(ship) {
+if (game.custom.fighters < 2) {
+    if (ship.custom.ready === false) {
+        ship.custom.invulnerable = false;
+        ship.custom.ready = true;
+        ship.set({
+          crystals: (((Math.trunc(ship.type / 100) * 100) /2) / 10)
+          * Math.trunc(ship.type / 100) * 4,
+          shield: 900,
+          generator: 900,
+          x:0,
+          y:0,
+          stats: 11111111 * Math.trunc(ship.type /100),
+          idle: true
+        });
+        change_component(ship,"READY [O]", "#11A716");
+        game.custom.fighters++;
+        ship.custom.fighter = true;
+    }
+    else if (ship.custom.ready === true) {
+        ship.custom.invulnerable = true;
+        ship.custom.ready = false;
+        ship.set({ 
+          crystals:
+            (((Math.trunc(ship.type / 100) * 100) /2) / 10) ,
+          stats: 0,
+          x: 500,
+          y: 0,
+          idle: false
+        });
+        game.custom.fighters--;
+      }
+    } 
+};
+
+
+var change_component = function(ship,message, color) {
+  ready.components[0].fill = color;
+  ready.components[1].value = message;
+  ship.setUIComponent(ready);
+};
+
+
+var re_tp = function(ship) {
+  ship.custom.ready = false;
+  ship.custom.invulnerable = true;
+  ship.set({
+    x: 500,
+    y: 0,
+    idle: false
+  })
+};
+this.event = function(event, game) {
+  let ship = event.ship;
+  let killer = event.killer;
+  switch (event.name) {
+    case "ui_component_clicked":
+      switch (event.id) {
+        case "reset_button":
+          reset(ship);
+          break;
+        case "ready":
+          ready_or_not(ship);
+          }
+          break;
+    case "ship_spawned":
+      if (ship !== null) {
+          respawn(ship);
+        }
+        break;
+    case "ship_destroyed":
+      if (ship !== null) {
+        if (killer !== null) {
+            re_tp(killer);
+            game.custom.fighters=0;
+          }
+        }
+        break;
+    }
+};
+
+
+game.modding.commands.test = function(req) {
+  echo("\nCurrent number of fighters in the arena: " + game.custom.fighters + "\n");
+}
+game.modding.commands.reset = function(req) {
+  game.custom.fighters = 0;
+  echo('\nNumber of fighters succesfully reseted to 0.\n')
+};
+
+
